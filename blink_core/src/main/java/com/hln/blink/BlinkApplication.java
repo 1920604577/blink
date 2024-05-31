2075f0a33c280a53c5bda270f1d3ff1fb3fcbb9e
hln <1920604577@qq.com>
2024-05-31 21:21:05 +0800

    test hln 2024-5-31-21:21

--- blink_core/src/main/java/com/hln/blink/BlinkApplication.java
+++ blink_core/src/main/java/com/hln/blink/BlinkApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlinkApplication {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(BlinkApplication.class, args);
     }
 }
